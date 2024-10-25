855e05c9525be601c0b6abdbc7efccfce34fca15
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-10-25 20:44:41 +0300

    added example router

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
 import express, { Request, Response, NextFunction } from 'express';
-import errorController from './controllers/errorController';
+import errorHandler from './middlewares/errorHandler';
 import httpLog from './middlewares/httpLog';
 import cookieParser from 'cookie-parser';
+import { notFoundEndpoint } from './middlewares/notAllowedHandler';
+import v1Router from './routes/v1/v1Router';
 
 const app = express();
 
-app.use(httpLog); // to log HTTP requests to http.log file and to the console
+app.use(httpLog); // to log HTTP requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use('/api/v1', v1Router); // example router
+
+app.all('*', notFoundEndpoint); // handle requests to endpoints that are not implemented
+
 // global error handler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  errorController(err, req, res, next);
+  errorHandler(err, req, res, next);
 });
 
 export default app;
