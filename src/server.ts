327421032adbf006d4fc91b8315591fa9b09e36b
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-10-25 20:45:09 +0300

    added configuration using dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
 import app from "./app";
+import dotenv from "dotenv";
+import logger from "./loggers/appLogger";
 
-app.listen(80, () => console.log('Server is listening on port 80'));
+dotenv.config({ path: './config' });
+
+const PORT: number = Number(process.env.PORT) || 80;
+
+app.listen(PORT, () => logger.info(`Server is listening on port ${PORT}`));
