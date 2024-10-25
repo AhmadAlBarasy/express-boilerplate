69cb219336ff07d15886f85986893b39875f9d8c
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-10-25 20:46:18 +0300

    Implemented app and http loggers

--- src/loggers/appLogger.ts
+++ src/loggers/appLogger.ts
@@ -1,15 +1,20 @@
 import winston, { format } from 'winston';
-const { combine, timestamp, json } = format;
+const { combine, timestamp, json, printf } = format;
+import { magenta } from 'colorette';
 
-const appLogger = winston.createLogger({
-  level: 'info', // change based on your needs or implement your own severity system
+const cliFormat =  printf(({level, message, timestamp}) => {
+  return `${magenta(`[time: ${timestamp}]`)} ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'info', // change based on your needs or use your own severity system
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.File({ filename: './logs/app.log' }),
   ]
 });
 
-if (process.env.NODE_ENV === 'development') 
-  appLogger.add(new winston.transports.Console());
+if (process.env.NODE_ENV !== 'production') 
+  logger.add(new winston.transports.Console({ format: combine(timestamp(), cliFormat) }));
 
-export default appLogger;
+export default logger;
--- src/loggers/httpLogger.ts
+++ src/loggers/httpLogger.ts
@@ -1,8 +1,13 @@
 import winston, { format } from 'winston';
-const { combine, timestamp, json } = format;
+const { combine, timestamp, json, printf } = format;
+import { magenta } from 'colorette';
+
+const cliFormat =  printf(({level, message, timestamp}) => {
+  return `${magenta(`[time: ${timestamp}]`)} ${level}: ${message}`;
+});
 
 const httpLogger = winston.createLogger({
-  level: 'http',
+  level: 'http', // change based on your needs or use your own severity system
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.File({ filename: './logs/http.log' }),
@@ -10,7 +15,7 @@ const httpLogger = winston.createLogger({
 });
 
 // log HTTP requests to the console if the node environment is 'development'
-if (process.env.NODE_ENV === 'development') 
-  httpLogger.add(new winston.transports.Console());
+if (process.env.NODE_ENV !== 'production') 
+  httpLogger.add(new winston.transports.Console({ format: combine(timestamp(), cliFormat) }));
 
 export default httpLogger;
