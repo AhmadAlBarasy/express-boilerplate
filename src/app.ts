08b810da394c80914dc5332da0616ae5a8273729
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-10-27 15:18:50 +0300

    Added security middlewares to the middleware chain

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import httpLog from './middlewares/httpLog';
 import cookieParser from 'cookie-parser';
 import { notFoundEndpoint } from './middlewares/notAllowedHandler';
 import v1Router from './routes/v1/v1Router';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
@@ -11,6 +13,14 @@ app.use(httpLog); // to log HTTP requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+/* security middlewares */
+app.use(helmet());
+app.use(rateLimit({ // configure based on your needs
+  windowMs: 60 * 1000,
+  limit: 50,
+}));
+
 
 app.use('/api/v1', v1Router); // example router
 
